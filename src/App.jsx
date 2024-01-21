d45833536cfd59bbec668e8b52237a6ace5c7ef5
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2024-01-21 17:18:25 +0100

    some files have been changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl text-center my-2 text-blue-800 font-semibold font-sans">
-        Welcome to the React Tailwind Template
+        Welcome to the React Tailwind Templates
       </h1>
     </>
   );
